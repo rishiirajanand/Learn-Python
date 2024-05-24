18b99de679b51cadadfef18ba069c5daa38b3f02
rishiirajanand <rishirajanand5@gmail.com>
2024-05-24 10:34:59 +0530

    day-1

--- Variable-Datatype/01-first.py
+++ Variable-Datatype/01-first.py
@@ -1,1 +1,34 @@
-print('Hello World')
+# Data-Type
+
+# a=10
+# b=20
+#
+# z =10
+# print(id(a), id(b), id(z))
+
+# name = "Rishi"
+# lName = 'Rishi'
+# print(id(name), id(lName))
+
+
+# String Concatenation
+
+str1 = 'abc'
+str2 = 'xyz'
+
+print(str1 + str2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
